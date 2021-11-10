1e8fe882b0cd7fda37dc95fb33640de86e8315f4
Marco Caimi <caimi.marco@protonmail.ch>
2021-11-10 16:23:52 +0100

    Add simple frontend

---
+++ src/main/resources/META-INF/resources/events_controller.js
@@ -0,0 +1,63 @@
+var app = angular.module("EventsManagement", []);
+  
+// generate UUID
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+//Controller Part
+app.controller("EventsManagementController", function ($scope, $http) {
+
+  //Initialize page with default data
+  $scope.form = {
+    id: create_UUID(),
+    message: "",
+    severity: ""
+  };
+
+  // HTTP POST Handler: Publish a new event
+  $scope.publish = function () {
+    var method = "";
+    var url = "";
+    var data = {};
+
+    // POST operation
+    method = "POST";
+    url = '/producer/post';
+    data.id = $scope.form.id;
+    data.message = $scope.form.message;
+    data.severity = $scope.form.severity;
+    data.event_timestamp = new Date().getTime().toString();
+
+
+    $http({
+      method: method,
+      url: url,
+      data: angular.toJson(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(_success, _error);
+  };
+
+  function _success(response) {
+    _clearForm()
+  }
+
+  function _error(response) {
+    alert(response.data.message || response.statusText);
+  }
+
+  // Clear the form
+  function _clearForm() {
+    $scope.form.message = "";
+    $scope.form.severity = "";
+    $scope.form.id = create_UUID();
+  }
+});
