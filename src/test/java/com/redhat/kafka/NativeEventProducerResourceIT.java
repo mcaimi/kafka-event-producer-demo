92e3d6218e5c6ea46d6e2a4f07226315c8a64ded
Marco Caimi <mcaimi@redhat.com>
2023-03-16 10:13:43 +0100

    Remove empty test case source files

--- src/test/java/com/redhat/kafka/NativeEventProducerResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.kafka;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeEventProducerResourceIT extends EventProducerResourceTest {
-
-    // Execute the same tests but in native mode.
-}
--- src/test/java/com/redhat/kafka/NativeVertxProducerResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.kafka;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeVertxProducerResourceIT extends VertxProducerResourceTest {
-
-    // Execute the same tests but in native mode.
-}
